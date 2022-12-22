eb76f5244374201fbc4cbf514e7c214fe466910d
Anton Polskiy <polskij.anton707@gmail.com>
2022-12-23 01:10:57 +0300

    Задача 4 Написал программу

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintStringArray(string[] array)
+{
+    foreach (string el in array)
+    {
+        System.Console.Write(el + " ");
+    }
+};
+
+
+string[] Solution(string[] array)
+{
+    string solutionInString = String.Empty;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4) solutionInString += (array[i] + ",");
+    }
+
+    return solutionInString.Split(',', StringSplitOptions.RemoveEmptyEntries);
+}
+
+
+
+string[] array = new string[] { "hello", "2", "world", ":-)" };
+
+PrintStringArray(Solution(array));
